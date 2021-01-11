539339226ccea5a30ee5fd47dc18b7deb70f6460
Gabriel <mail@gabrieldavison.me.uk>
2021-01-11 15:56:18 +0000

    Add iphone user agent detection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import Hydra from "hydra-synth";
 import { initialScene } from "./hydra";
+import iOS from "./utils/iOS";
 
 window.onload = () => {
-  const hydra = new Hydra({
-    canvas: document.getElementById("hydra-canvas"),
-    detectAudio: false,
-    enableStreamCapture: false,
-  });
-  hydra.setResolution(854, 480);
-  initialScene();
+  if (!iOS()) {
+    const hydra = new Hydra({
+      canvas: document.getElementById("hydra-canvas"),
+      detectAudio: false,
+      enableStreamCapture: false,
+    });
+    hydra.setResolution(854, 480);
+    initialScene();
+  }
 };
--- src/utils/iOS.js
+++ src/utils/iOS.js
@@ -1,14 +1,18 @@
+// export default function iOS() {
+//   return (
+//     [
+//       "iPad Simulator",
+//       "iPhone Simulator",
+//       "iPod Simulator",
+//       "iPad",
+//       "iPhone",
+//       "iPod",
+//     ].includes(navigator.platform) ||
+//     // iPad on iOS 13 detection
+//     (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+//   );
+// }
+
 export default function iOS() {
-  return (
-    [
-      "iPad Simulator",
-      "iPhone Simulator",
-      "iPod Simulator",
-      "iPad",
-      "iPhone",
-      "iPod",
-    ].includes(navigator.platform) ||
-    // iPad on iOS 13 detection
-    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-  );
+  return !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent);
 }
