8803692c3262c92f00664e6f73927ee622e3398b
Gabriel <mail@gabrieldavison.me.uk>
2021-01-12 14:05:16 +0000

    Add credits page

---
+++ src/credits/index.js
@@ -0,0 +1,12 @@
+import Hydra from "hydra-synth";
+import hydraIndex from "../hydraIndex";
+
+window.onload = () => {
+  const hydra = new Hydra({
+    canvas: document.getElementById("hydra-canvas"),
+    detectAudio: false,
+    enableStreamCapture: false,
+  });
+  hydra.setResolution(854, 480);
+  hydraIndex();
+};
