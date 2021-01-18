80ec6f74a3dce7f932094abc380fd4bf7071e806
Gabriel <mail@gabrieldavison.me.uk>
2021-01-18 12:55:13 +0000

    Set up page2 scene

--- src/page2/index.js
+++ src/page2/index.js
@@ -1,1 +1,6 @@
-import p5 from "p5.js";
+import p5 from "p5";
+import p5Sketch from "./p5Sketch";
+
+window.onload = () => {
+  const sketch = new p5(p5Sketch);
+};
--- src/page2/p5Sketch.js
+++ src/page2/p5Sketch.js
@@ -1,9 +1,49 @@
-import p5 from "p5.js";
+let maskUrl = require("../assets/mask1-hp.obj");
 
 const p5Sketch = (p) => {
-  p.preload = function () {};
+  let canvas;
+  let mask;
+  let cam;
 
-  p.setup = function () {};
+  let camX = 0;
+  let camY = 0;
+  let camZ = p.height / 2.0 / p.tan((p.PI * 30.0) / 180.0);
+  let camCenterX = 0;
+  let camCenterY = 0;
+  let camCenterZ = 0;
 
-  p.draw = function () {};
+  p.preload = function () {
+    mask = p.loadModel(maskUrl);
+  };
+
+  p.setup = function () {
+    canvas = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+    let cam = p.camera();
+    camZ = p.height / 2.0 / p.tan((p.PI * 30.0) / 180.0);
+  };
+
+  p.draw = function () {
+    p.background(100);
+    camX = p.map(p.mouseX, 0, p.width, 500, -500);
+    camY = p.map(p.mouseY, 0, p.height, 500, -500);
+    p.camera(camX, camY, camZ, camCenterX, camCenterY, camCenterZ, 0, 1, 0);
+    p.push();
+    p.rotateX(p.HALF_PI);
+    p.translate(0, -150, -250);
+    p.noFill();
+    p.normalMaterial();
+    p.plane(1000, 1000);
+
+    p.pop();
+
+    p.push();
+    p.scale(50);
+    p.rotateZ(p.PI);
+    p.noFill();
+    p.normalMaterial();
+    p.model(mask);
+    p.pop();
+  };
 };
+
+export default p5Sketch;
