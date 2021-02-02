4c24621d4987b9b1df4c18aa3c37242118db8b73
Gabriel <mail@gabrieldavison.me.uk>
2021-02-02 18:45:23 +0000

    remove extra files

--- src/ghosts-of-my-life/hydraSketch.js
+++ src/ghosts-of-my-life/hydraSketch.js
@@ -1,10 +1,4 @@
 export default function hydraSketch() {
-  // osc().modulateRotate(o0, 0.3).out();
-  // osc(33, 0.3, 0.3).diff(o3, 3).out(o1);
-  // osc(3, 0.3, 33).modulateKaleid(o3, 3).diff(o0).out(o2);
-  // src(o0, 3).mult(o1, 3).kaleid(3).out(o3);
-  // render(o2);
-
   osc()
     .modulateRotate(o0, () => mouse.x / 7000)
     .out();
